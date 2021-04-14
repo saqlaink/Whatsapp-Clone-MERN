13472e629d91fda85ee7593240ce1a20430cb316
Saqlain Khan <iamsaqlain25@gmail.com>
2021-04-14 16:08:43 +0530

    First

---
+++ whatsapp-frontend/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUH5To5KVgUy0WSrTOiChCEZL47O0hyAo",
+  authDomain: "whatsapp-mern-7e234.firebaseapp.com",
+  projectId: "whatsapp-mern-7e234",
+  storageBucket: "whatsapp-mern-7e234.appspot.com",
+  messagingSenderId: "635525859697",
+  appId: "1:635525859697:web:12b5602f7a5319f8520ed0",
+};
